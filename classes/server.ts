e20816a2606d3d30c075932a920629e395e99767
William Zamora <williamzv@gmail.com>
2019-01-09 16:02:10 -0600

    Actualizacion de los sockets

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,43 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
-import { timingSafeEqual } from 'crypto';
+import socketIO from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/socket';
 
 export default class Server {
     public app: express.Application;
     public port: number;
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+    private static _instance: Server;
 
-    constructor() {
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+
+        this.escucharSockets();
     }
 
     start(callback: Function) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
+    }
+    
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    private escucharSockets() {
+        console.log('Escuchando conexiones - sockets');
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+
+            // Mensajes
+            socket.mensaje(cliente, this.io);
+
+            // Desconectar
+            socket.desconectar(cliente);
+        });
     }
 }
