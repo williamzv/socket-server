d6bdeaa6f98637900c17d9b509d937409a8067ff
William Zamora <williamzv@gmail.com>
2019-01-11 19:58:31 -0600

    Mensajes Globales y Privados

--- classes/server.ts
+++ classes/server.ts
@@ -31,13 +31,18 @@ export default class Server {
     private escucharSockets() {
         console.log('Escuchando conexiones - sockets');
         this.io.on('connection', cliente => {
-            console.log('Cliente conectado');
+            // Conectar cliente
+            socket.conectarCliente(cliente);
 
+            // Configurar usuario
+            socket.configurarUsuario(cliente, this.io);
+            
             // Mensajes
             socket.mensaje(cliente, this.io);
 
             // Desconectar
             socket.desconectar(cliente);
+
         });
     }
 }
---
+++ classes/usuarios-lista.ts
@@ -0,0 +1,47 @@
+import { Usuario } from './usuario';
+export class UsuariosLista {
+    private lista: Usuario[] = [];
+
+    constructor() {}
+
+    // Agregar un usuario
+    public agregar(usuario: Usuario) {
+        this.lista.push(usuario);
+        console.log(this.lista);
+        return usuario;
+    }
+
+    public actualizarNombre(id: string, nombre: string) {
+        for( let usr of this.lista) {
+            if (usr.id === id) {
+                usr.nombre = nombre;
+                break;
+            }
+        }
+        console.log('Actualizando usuario');
+        console.log(this.lista);
+    }
+
+    // Obtener lista de usuario
+    public getLista() {
+        return this.lista;
+    }
+
+    // Obtener un usuario
+    public getUsuario(id: string) {
+        return this.lista.find(usuario => usuario.id === id);
+    }
+
+    // Obtener todos los usuarios en una sala en particular
+    public getUsuariosSala(sala: string) {
+        return this.lista.filter(usuario => usuario.sala === sala);
+    }
+
+    // Borrar un usuario cuando deja la sala
+    public borrarUsuario(id: string) {
+        const tmpUsuario = this.getUsuario(id);
+        this.lista = this.lista.filter(usuario => usuario.id !== id);
+        return tmpUsuario;
+    }
+
+}
